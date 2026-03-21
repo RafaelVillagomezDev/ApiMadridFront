c7bb2fec37c5333190c2d961557e163348098e82
Yandry Villagomez <rafaeldev.villagomez@gmail.com>
2026-03-21 18:02:21 +0100

    nuevo router componente

--- ZappyMap/src/core/components/base/Nav.vue
+++ ZappyMap/src/core/components/base/Nav.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
+import type { RouterCustomLink } from '@/types/router-types';
+import { RouterLink } from 'vue-router';
+
+
+interface RouterNavbar {
+    routerCustom: RouterCustomLink[]
+}
+
+defineProps<RouterNavbar>();
 
 </script>
 
 <template>
-    <h1 class="flex">navbar</h1>
+    <nav class="flex flex-row gap-1">
+        <RouterLink class="text-slate-900" v-for="item in routerCustom" :key="item.linkCustom" :to="item.linkCustom">
+            {{ item.linkTitle }}
+        </RouterLink>
+    </nav>
 </template>
---
+++ ZappyMap/src/types/router-types.ts
@@ -0,0 +1,6 @@
+export interface RouterCustomLink {
+
+    linkCustom: string,
+    linkTitle: string
+
+}
